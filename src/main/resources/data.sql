9cbd60e2ba640966de7d2d1646168e1c05b4b67f
vandanajaiswal <vandanajaiswal@nbnco.com.au>
2018-12-08 23:22:48 +1100

    removed unwanted files

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO person VALUES (1, 'Vandana', 'Jaiswal', '0407681729', 'vj@jais.org');
 INSERT INTO person VALUES (2, 'Bill', 'Shorten', '123456789', 'bill@jais.org');
 
-INSERT INTO book VALUES (1,'More Effective C++','Meyers, Scott','0785342633719', 1);
-INSERT INTO book VALUES (2,'Imajica','Barker, Clive','9780006178040', 1);
+INSERT INTO book VALUES (1,'Adventures of La La Island','M, Scott','0785342633719', 1);
+INSERT INTO book VALUES (2,'Jamaica','Jen, Clive','9780006178040', 1);
